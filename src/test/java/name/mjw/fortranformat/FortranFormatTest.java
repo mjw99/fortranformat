050b5e49a9a087ec63006b00c3ca51b371f151c5
Mark J. Williamson <mw529@cam.ac.uk>
2019-04-25 14:33:19 +0100

    Revert "Migrate to JUnit 5"
    This reverts commit 5d58634439b6653b14932856db716181040ca1b7.
    
    Needed for JDK 7 support

--- src/test/java/name/mjw/fortranformat/FortranFormatTest.java
+++ src/test/java/name/mjw/fortranformat/FortranFormatTest.java
@@ -1,9 +1,8 @@
 package name.mjw.fortranformat;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
